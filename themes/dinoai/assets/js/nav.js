924c2d54dc9d73d18ee91e2f68b39473d2f756a1
Dino Ratcliffe <dr@dino.ai>
2025-03-02 09:21:51 +0000

    Adjusted menu, added homepage and adjusted fonts.

--- themes/dinoai/assets/js/nav.js
+++ themes/dinoai/assets/js/nav.js
@@ -8,31 +8,10 @@ class DinoAINav extends HTMLElement {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = `
             <style>
-            .nav_list {
-                color: var(text-colour)!important;
-            }
             .nav-list ::slotted(.iron-selected) {
-                color: rgba(255, 255, 255, 0.2)!important;
-                background-color: var(--menu-selected);
-                border: solid 0.2em var(--menu-selected);
+                color: rgba(0, 0, 0, 0.2)!important;
             }
 
-            .nav-list ::slotted(a.iron-selected)::before {
-                content: "-"!important;
-                color: rgba(255, 255, 255, 0.2)!important;
-            }
-            .nav-list ::slotted(a.iron-selected)::after {
-                content: "-"!important;
-                color: rgba(255, 255, 255, 0.2)!important;
-            }
-            .nav-list ::slotted(a.iron-selected:hover)::before {
-                content: "-"!important;
-                color: rgba(255, 255, 255, 0.2)!important;
-            }
-            .nav-list ::slotted(a.iron-selected:hover)::after {
-                content: "-"!important;
-                color: rgba(255, 255, 255, 0.2)!important;
-            }
 
 
             .nav-list ::slotted(a:hover) {
@@ -49,20 +28,20 @@ class DinoAINav extends HTMLElement {
                 transition: border-bottom 0.5s, background-color 0.5s, color 0.5s, opacity: 0.5s;
                 font-family: 'Gothic A1', sans-serif;
                 text-decoration: none;
-                flex: 1;
                 height: 1.5em;
                 line-height: 1.5em;
                 color: var(text-colour)!important;
                 opacity: 1;
                 text-transform: uppercase;
                 cursor: pointer;
+                flex: 1 1 auto;
             }
             @media screen and (max-width: 900px) {
                 .nav-list ::slotted(a) {
                     display: block;
                     text-align: center;
-		    padding: 0.3em;
-		    font-size: 1.2em;
+                    padding: 0.3em;
+                    font-size: 1.2em;
                 }
                 .nav-list {
                     padding-bottom: 0em;
@@ -70,7 +49,7 @@ class DinoAINav extends HTMLElement {
             }
             </style>
             <div id="nav-top">
-                <div class="nav-list" role="navigation">
+                <div class="nav-list" role="navigation" style="display: flex;justify-content: space-between">
                     <slot></slot>
                 </div>
             </div>
--- themes/dinoai/content/_index.md
+++ themes/dinoai/content/_index.md
@@ -3,5 +3,3 @@ title = 'Home'
 date = 2023-01-01T08:00:00-07:00
 draft = false
 +++
-
-# Place some things here for the home page
